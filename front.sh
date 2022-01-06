e97c5d80ec5812cdc31981e8a1bf34f310c1c0c2
Peter Yuen <ppeetteerrs@Peter.localdomain>
2022-01-06 20:03:24 +0800

    Added frontmatter fix

--- front.sh
+++ front.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 for filename in $(pwd)/content/*.md; do
-    start=$(cat "$filename" | head -c 3);
-	if [ "$start" != "---" ]; then
-		echo "Appending front matter to" "$filename"
-		echo "---\naliases: []\n---\n$(cat "$filename")" > "$filename"
+	if [[ "$filename" != *"index.md" ]]; then
+		start=$(cat "$filename" | head -c 3);
+		echo ""
+		echo "========> Appending front matter to" "$filename"
+		content=$(sed '1 { /^---/ { :a N; /\n---/! ba; d} }' "$filename")
+		modified=$(date -Rr "$filename")
+		echo "---\nupdated:" "$modified" "\n---\n$content" # > "$filename"
 	fi
 done
