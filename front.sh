bc6c00d818dfd2260309811adbefe7b06d4980ee
Peter Yuen <ppeetteerrs@Peter.localdomain>
2022-01-06 19:16:04 +0800

    Added frontmatter fix

--- front.sh
+++ front.sh
@@ -4,6 +4,6 @@ for filename in $(pwd)/content/*.md; do
     start=$(cat "$filename" | head -c 3);
 	if [ "$start" != "---" ]; then
 		echo "Appending front matter to" "$filename"
-		echo -e "---\naliases: []\n---\n$(cat "$filename")" > "$filename"
+		echo "---\naliases: []\n---\n$(cat "$filename")" > "$filename"
 	fi
 done
